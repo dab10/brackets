6ab8ae14f2edc6ab8043d655d068593771aec889
dab10 <dab10@list.ru>
2022-01-30 17:10:03 +0300

    feat: check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    OPEN_BRACKETS.push(bracketsConfig[i][0])
+  }   
+
+  const BRACKETS_PAIR = {}
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  
+     
+      let stack = [];
+  
+      for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+        
+        if (OPEN_BRACKETS.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+        } else {
+          if (stack.length === 0) {
+            return false;
+          }
+  
+          let topElement = stack[stack.length - 1];
+  
+          if (BRACKETS_PAIR[currentSymbol] === topElement) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+  
+      return stack.length === 0;
 }
